1adb2fa0b9aebd637dce2a636f7dd416053113aa
mushfiq <mushfiq.moon@gmail.com>
2013-10-16 15:52:55 +0200

    added demo version of rssi REST api using tastypie using mongoengine and also insterted test data into the database

---
+++ restRSSI/restApp/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'restApp.views.home', name='home'),
+#     # url(r'^restApp/', include('restApp.foo.urls')),
+# 
+#     # Uncomment the admin/doc line below to enable admin documentation:
+#     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+# 
+#     # Uncomment the next line to enable the admin:
+#     # url(r'^admin/', include(admin.site.urls)),
+# )
+from tastypie import api
+from restApp.resources import RssiResource
+
+v1_api = api.Api(api_name='v1')
+v1_api.register(RssiResource())
+urlpatterns = patterns('',
+    url(r'^api/', include(v1_api.urls)),
+)
