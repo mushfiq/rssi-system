9f84feebe0f26e01d416a7234bd8f1f90b78bb27
mushfiq <mushfiq.moon@gmail.com>
2013-10-28 00:41:25 +0100

    added test structure and coverage

---
+++ RESTApi/restApp/models.py
@@ -0,0 +1,1 @@
+#file for conf checking error escaping
--- RESTApi/restApp/settings.py
+++ RESTApi/restApp/settings.py
@@ -153,6 +153,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'restApp',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
