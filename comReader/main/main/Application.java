5976aa0f42e5b7706b5322d4e55c4b2557fdd0ff
Danilo <Danilo@Danilo-PC>
2013-11-01 10:47:02 +0100

    Changed "Application" class

--- comReader/main/main/Application.java
+++ comReader/main/main/Application.java
@@ -2,4 +2,18 @@ package main;
 
 public class Application {
 
+	private static Application application;
+	
+	private Application(){
+		
+	}
+	
+	public static Application getApplication(){
+		
+		if(application == null){
+			application = new Application();
+		}
+		
+		return application;
+	}
 }
