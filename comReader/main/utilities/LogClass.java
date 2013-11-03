87e91f584b9da79555cad5fa95a04a82055f1ffc
Danilo <Danilo@Danilo-PC>
2013-11-03 13:43:39 +0100

    Added logging library and capabilities.

---
+++ comReader/main/utilities/LogClass.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.SimpleLayout;
+
+
+public class LogClass {
+	
+   
+	public static Logger logger = Logger.getLogger(LogClass.class);
+	 
+	public static void main(String[] args) {
+		
+	}
+}
