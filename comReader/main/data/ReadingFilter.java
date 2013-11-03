7ba427993215788b2d03a3ff7b721b387568eb4a
Danilo <Danilo@Danilo-PC>
2013-11-03 14:02:34 +0100

    Code refactoring.

--- comReader/main/data/ReadingFilter.java
+++ comReader/main/data/ReadingFilter.java
@@ -1,28 +1,48 @@
 package data;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ReadingFilter.
+ */
 public class ReadingFilter {
 
+	/** The data. */
 	private Reading data;
 	
+	/**
+	 * Instantiates a new reading filter.
+	 */
 	public ReadingFilter() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Instantiates a new reading filter.
+	 *
+	 * @param data the data
+	 */
 	public ReadingFilter(Reading data) {
 		super();
 		if (data == null) {
 			this.data = new Reading();
-		}
-		else{
+		} else {
 			this.data = data;
 		}
 		
 	}
 	
+	/**
+	 * Removes the inappropriate values.
+	 */
 	public void removeInappropriateValues() {
 		
 	}
 	
+	/**
+	 * Calculate average.
+	 *
+	 * @return the int
+	 */
 	public int calculateAverage() {
 		
 		return 0;
