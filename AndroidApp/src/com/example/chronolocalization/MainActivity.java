f2bade79b46131133d88cff7ceb04146ac474d33
maheswarir <rmaheswarimca@gmail.com>
2013-11-05 15:06:24 +0100

    added new print statement just for test

--- AndroidApp/src/com/example/chronolocalization/MainActivity.java
+++ AndroidApp/src/com/example/chronolocalization/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		// just a simple comment
 		//Connects the Application with the Data Source (at the moment a dummy implementation)
 		dataManager = new DataManager();
 
