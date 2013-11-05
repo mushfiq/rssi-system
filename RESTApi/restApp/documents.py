816532c747166720f75e21964fda17f1fb5b5161
mushfiq <mushfiq.moon@gmail.com>
2013-11-05 18:05:18 +0100

    added WatchResource documents and added new resources to handle new documents updated the dummy script for inserting dummy data into the mongo

--- RESTApi/restApp/documents.py
+++ RESTApi/restApp/documents.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, EmbeddedDocument, StringField
+from mongoengine import Document, EmbeddedDocument, StringField, FloatField, DateTimeField, IntField
 
 class InheritableDocument(Document):
     meta = {
@@ -16,3 +16,8 @@ class rawData(InheritableDocument):
     signalStrenth = StringField(max_length=200, required=True)
     insertAt = StringField(max_length=40, required=True)
     
+class watchRecords(InheritableDocument):
+	x = FloatField()
+	y = FloatField()
+	insertedAt = DateTimeField()
+	mapId = IntField()
--- RESTApi/restApp/dummydata.py
+++ RESTApi/restApp/dummydata.py
@@ -1,7 +1,7 @@
 import time
-import datetime
-from random import shuffle, random
-from documents import rawData
+from datetime import datetime
+from random import shuffle, random, uniform
+from documents import rawData, watchRecords
 from  mongoengine import connect, Document
 
 connect('rssiSystem')
@@ -36,7 +36,20 @@ def addRadomData():
 	dObj.save()
 	print "saved"
 	
+def updatedDummyData():
+	dObj = watchRecords()
+	dObj.x = uniform(1.2,4.5)
+	dObj.y = uniform(100.01, 302.5)
+	dObj.insertedAt = datetime.now()
+	try:
+		dObj.save()
+		print "Saved"
+	except Exception, e:
+		print e
+	
 if __name__ == '__main__':
+	for i in range(0, 10):
+		updatedDummyData()
 	# insertDummyData()
-	for i in range(0, 3):
-	    addRadomData()
+	# for i in range(0, 3):
+	#     addRadomData()
