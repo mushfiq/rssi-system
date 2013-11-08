b48c142edfc94a2e0e86d0880154965524c244f4
Danilo <Danilo@Danilo-PC>
2013-11-08 10:49:50 +0100

    Added COMPortDataReader class.

---
+++ comReader/main/data/COMPortDataReader.java
@@ -0,0 +1,15 @@
+package data;
+
+public class COMPortDataReader extends DataReader {
+
+	public COMPortDataReader() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void readData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
