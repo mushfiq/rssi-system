eb90e3fe4181dc5dabab46e17de293faaf2f5415
shelbyi <tommy.griese@gmail.com>
2013-11-10 21:34:39 +0100

    *code cleaning (not used methods deleted, refactored) *create JavaDoc and applied checkstyle

--- comReader/main/algorithm/helper/PointProbabilityMap.java
+++ comReader/main/algorithm/helper/PointProbabilityMap.java
@@ -1,25 +1,60 @@
+/*
+ * File: PointProbabilityMap.java
+ * Date				Author				Changes
+ * 08 Nov 2013		Tommy Griese		create version 1.0
+ */
 package algorithm.helper;
 
-
+/**
+ * The class PointProbabilityMap represents a special point in a probability map. 
+ * Each point has a variable that represents the rssi value in this point
+ * (this class is inherited by class {@link algorithm.helper.Point}).
+ * 
+ * @version 1.0 08 Nov 2013
+ * @author Tommy Griese
+ * @see algorithm.helper.Point
+ */
 public class PointProbabilityMap extends Point {
 
-	public double rssi_value;
+	/** The rssi value in this point. */
+	private double rssiValue;
         
-	public PointProbabilityMap(double x, double y, double rssi_value) {
+	/**
+	 * Instantiates a new PointProbabilityMap.
+	 *
+	 * @param x the x coordinate of the point
+	 * @param y the y coordinate of the point
+	 * @param rssiValue the rssi value in this point
+	 */
+	public PointProbabilityMap(double x, double y, double rssiValue) {
 		super(x, y);
-        this.rssi_value = rssi_value;
+        this.rssiValue = rssiValue;
 	}
-        
-        // Copy-Konstruktor
-    public PointProbabilityMap(PointProbabilityMap p) {
-            this(p.x, p.y, p.rssi_value);
-    }
     
-    public PointProbabilityMap reversed() {
-            return new PointProbabilityMap(-this.x, -this.y, this.rssi_value);
-    }
-    
+	/**
+	 * Gets the rssi value in this point.
+	 *
+	 * @return the rssi value
+	 */
+	public double getRSSIValue() {
+		return this.rssiValue;
+	}
+	
+	/**
+	 * Sets the rssi value in this point.
+	 *
+	 * @param rssiValue the new rssi value
+	 */
+	public void setRSSIValue(double rssiValue) {
+		this.rssiValue = rssiValue;
+	}
+	
+	/**
+	 * Represents a string of this point.
+	 *
+	 * @return a string
+	 */
     public String toString() {
-        return "x = " + super.x + " y = " + super.y + " rssi = " + this.rssi_value;
+        return "[" + this.getX() + ";" + this.getY() + "] - rssi = " + this.rssiValue;
     }
 }
