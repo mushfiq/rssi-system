e6d746e31b89643329ac8a2ea363398d7f9cbe0f
EichmannSilvio <Eichmann.Silvio@gmail.com>
2013-11-11 11:58:02 +0100

    Code Cleaning in AndroidApp

--- AndroidApp/gen/com/example/chronolocalization/R.java
+++ AndroidApp/gen/com/example/chronolocalization/R.java
@@ -31,16 +31,13 @@ public final class R {
         public static final int room_svg=0x7f020004;
     }
     public static final class id {
-        public static final int CurrentPositionText=0x7f090005;
-        public static final int PositionText=0x7f090004;
+        public static final int CurrentPositionText=0x7f090004;
+        public static final int PositionText=0x7f090003;
         public static final int SelectWatch=0x7f090000;
         public static final int WatchSpinner=0x7f090001;
-        public static final int action_settings=0x7f090009;
-        public static final int displayLastNPositions=0x7f090007;
-        public static final int displayPosition=0x7f090006;
-        public static final int editURL=0x7f090002;
-        public static final int mapImage=0x7f090003;
-        public static final int numberOfSteps=0x7f090008;
+        public static final int action_settings=0x7f090006;
+        public static final int displayPosition=0x7f090005;
+        public static final int mapImage=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
