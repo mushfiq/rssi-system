ee72bb477f7be1ce94bc62c4a8249ac3413709c5
Mushfiq <mushfiq.moon@gmail.com>
2013-11-24 01:38:20 +0100

    added basic structure for API resource test cases

--- RESTApi/restApp/settings.py
+++ RESTApi/restApp/settings.py
@@ -158,6 +158,7 @@ INSTALLED_APPS = (
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
