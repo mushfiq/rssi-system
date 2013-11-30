73f5a0fc4b5c0f312c51cc6db692322adf5b2d18
Mushfiq <mushfiq.moon@gmail.com>
2013-11-30 01:17:12 +0100

    added access key restriction for all resources

--- RESTApi/restApp/resources.py
+++ RESTApi/restApp/resources.py
@@ -1,13 +1,12 @@
 # from tastypie import authorization as tastypie_authorization, fields as tastypie_fields
-from tastypie.authorization import Authorization
-
 from django.views.decorators.csrf import csrf_exempt
 
+# from tastypie.authorization import Authorization
 from tastypie_mongoengine import paginator, resources
-
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 
 from restApp import documents
+from rssi_auth import RssiAuth
 
 
 class WatchResource(resources.MongoEngineResource):
@@ -26,22 +25,23 @@ class WatchResource(resources.MongoEngineResource):
 
         excludes = ['resource_uri']
 
-        authorization = Authorization()
+        authentication = RssiAuth()
+        
         resource_name = 'watch'
 
 class MapResource(resources.MongoEngineResource):
     class Meta:
         queryset = documents.mapRecords.objects.order_by('mapId')
+        
+        resource_name = 'map'
 
-        
-        
         allowed_methods = ('get', 'post', 'put', 'delete')
         
         filtering = {
         	"mapId"		 : ['exact',],
         }
 
-        authorization = Authorization()
+        authentication = RssiAuth()
 
         excludes = ['resource_uri']
     
@@ -50,7 +50,7 @@ class ReceiverResource(resources.MongoEngineResource):
     class Meta:
         queryset = documents.receiverRecords.objects.all()
         
-        # resource_name = 'receiver'
+        resource_name = 'receiver'
         
         allowed_methods = ('get', 'post', 'put', 'delete')
         
@@ -58,7 +58,7 @@ class ReceiverResource(resources.MongoEngineResource):
         	"mapId"		 : ['exact',],
         }
         
-        authorization = Authorization()
+        authentication = RssiAuth()
         
         paginator_class = paginator.Paginator
         
