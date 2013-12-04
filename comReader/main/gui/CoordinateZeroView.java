1875ee1a3c5c543e149c5a1ad77d7547a4d27871
daniloradenovic <dacha111@gmail.com>
2013-12-04 10:48:39 +0100

    Added class 'CoordinateZeroView' which will be a visual representation of (0,0) coordinate point on the map.

---
+++ comReader/main/gui/CoordinateZeroView.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.JComponent;
+
+public class CoordinateZeroView extends JComponent {
+
+	public CoordinateZeroView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
