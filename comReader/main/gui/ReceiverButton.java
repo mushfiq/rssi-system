b30522c8582d1800febf083100aa49562929118a
daniloradenovic <dacha111@gmail.com>
2013-12-04 10:49:33 +0100

    Added 'ReceiverButton' and 'ReceiverButtonState' classes and their image icons.

---
+++ comReader/main/gui/ReceiverButton.java
@@ -0,0 +1,113 @@
+package gui;
+
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import components.Receiver;
+
+public class ReceiverButton extends JButton {
+
+	private static final long serialVersionUID = 1L;
+	private static Image addReceiverToMapImage;
+	private static Image removeReceiverFromMapImage;
+	private Receiver receiver;
+	private ReceiverButtonState state;
+	private ReceiverView receiverView;
+	private static final String PLUS_IMAGE = "images/plus.png";
+	private static final String MINUS_IMAGE = "images/minus.png";
+	private AddMapDialog addMapDialog;
+
+	public ReceiverButton(Receiver receiver, AddMapDialog parent) {
+
+		this.receiver = receiver;
+		this.addMapDialog = parent;
+		initialize();
+	}
+
+	private void initialize() {
+		ReceiverButtonState buttonState = (receiver.isOnMap()) ? ReceiverButtonState.REMOVE
+				: ReceiverButtonState.ADD;
+		addActionListener(new ReceiverButtonListener());
+		addReceiverToMapImage 	   = loadImage(PLUS_IMAGE);
+		removeReceiverFromMapImage = loadImage(MINUS_IMAGE);
+		setState(buttonState);
+	}
+
+	public ReceiverButtonState getState() {
+		return state;
+	}
+
+	public void setState(ReceiverButtonState state) {
+		this.state = state;
+		refreshIcon();
+	}
+
+	private void refreshIcon() {
+
+		if (state == ReceiverButtonState.ADD) {
+			
+			this.setText(receiver.getID() + "");
+			this.setIcon(new ImageIcon(addReceiverToMapImage));
+		} else {
+			
+			this.setText(receiver.getID() + "");
+			this.setIcon(new ImageIcon(removeReceiverFromMapImage));
+		}
+	}
+
+	private void toggle() {
+		
+		if (this.state == ReceiverButtonState.ADD) {
+			//MapPreviewPanel.addReceiverView(new ReceiverView);
+			addMapDialog.addReceiverToMap(receiver);
+			
+		} else { // it is REMOVE
+			//MapPreviewPanel.removeReceiverView(Receiver);
+			addMapDialog.removeReceiverFromMap(receiver);
+		}
+		
+		ReceiverButtonState newState = (this.state == ReceiverButtonState.ADD) ? ReceiverButtonState.REMOVE
+				: ReceiverButtonState.ADD;
+		this.setState(newState);
+		refreshIcon();
+		
+	}
+
+	private Image loadImage(String relativePath) {
+		
+		String path = this.getClass().getResource(relativePath)
+				.getPath();
+		Image myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return myPicture;
+	}
+	
+	
+	private class ReceiverButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (!(e.getSource() instanceof ReceiverButton)) { // not a button
+				return;
+			}
+
+			ReceiverButton button = (ReceiverButton) e.getSource();
+			button.toggle();
+		}
+	}
+
+}
