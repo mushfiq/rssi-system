5e19a71fdaf49555666762d34b034942397e5a94
daniloradenovic <dacha111@gmail.com>
2013-12-10 16:54:09 +0100

    Added combo box for choosing the algorithm.

--- comReader/main/gui/ParametersPanel.java
+++ comReader/main/gui/ParametersPanel.java
@@ -7,27 +7,49 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.logging.Logger;
 
+import javax.swing.ComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 
+import algorithm.PositionLocalizationAlgorithm;
+import main.Application;
+import utilities.Utilities;
+
 public class ParametersPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	private static final int PANEL_WIDTH = 200;
-	private static final int PANEL_HEIGHT = 550;
+	private static final int PANEL_HEIGHT = 620;
+	private static final int ROTATE_AMOUNT = 45; // degrees
+	private static final String ROTATE_CLOCKWISE_IMAGE_PATH = "images/clockwise.png";
+	private static final String ROTATE_COUNTER_CLOCKWISE_IMAGE_PATH = "images/counterClockwise.png";
+	private static final String START_ICON_IMAGE_PATH = "images/start.png";
+	private static final String STOP_ICON_IMAGE_PATH = "images/stop.png";
 	private JButton uploadButton, saveButton, cancelButton;
 	private JLabel ratioLabel;
 	private JLabel animationLabel;
 	private JSpinner ratioSpinner;
 	private AddMapDialog addMapDialog;
+	private JButton rotateReceiverClockwiseButton;
+	private JButton rotateReceiverCounterClockwiseButton;
+	private StartStopButton startStopButton;
+	private JComboBox<PositionLocalizationAlgorithmType> algorithmComboBox;
+	/** The logger. */
+	private Logger logger;
+	private AddMapDialogMode openningMode;
 	
-	public ParametersPanel(AddMapDialog parentDialog) {
+	public ParametersPanel(AddMapDialog parentDialog, AddMapDialogMode openningMode) {
 		
+		logger = Utilities.initializeLogger(this.getClass().getName());
+		this.openningMode = openningMode;
 		this.addMapDialog = parentDialog;
 		initialize();
 		addListenersToComponents();
@@ -39,6 +61,68 @@ public class ParametersPanel extends JPanel{
 		setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
 		this.setLayout(new GridBagLayout());
 		setBackground(new Color(230, 230, 230));
+		
+		// Add 'Counter clockwise button'
+		ImageIcon counterClockwiseIcon = new ImageIcon(Utilities.loadImage(ROTATE_COUNTER_CLOCKWISE_IMAGE_PATH));
+		rotateReceiverCounterClockwiseButton = new JButton(counterClockwiseIcon);
+		rotateReceiverCounterClockwiseButton.addActionListener(new RotateButtonsListener());
+		GridBagConstraints gbc4 = new GridBagConstraints();
+		gbc4.gridx = 0;
+		gbc4.gridy = 3;
+		gbc4.gridwidth = 1;
+		gbc4.gridheight = 1;
+		gbc4.weightx = 1;
+		gbc4.weighty = 2;
+		gbc4.anchor = GridBagConstraints.BASELINE_LEADING;
+		
+		this.add(rotateReceiverCounterClockwiseButton, gbc4);
+		
+		// Add 'Clockwise button'
+		ImageIcon clockwiseIcon = new ImageIcon(Utilities.loadImage(ROTATE_CLOCKWISE_IMAGE_PATH));
+		rotateReceiverClockwiseButton = new JButton(clockwiseIcon);
+		rotateReceiverClockwiseButton.addActionListener(new RotateButtonsListener());
+		GridBagConstraints gbc5 = new GridBagConstraints();
+		gbc5.gridx = 1;
+		gbc5.gridy = 3;
+		gbc5.gridwidth = 1;
+		gbc5.gridheight = 1;
+		gbc5.weightx = 1;
+		gbc5.weighty = 2;
+		gbc5.anchor = GridBagConstraints.BASELINE_TRAILING;
+		
+		this.add(rotateReceiverClockwiseButton, gbc5);
+		
+		
+		// add start/stop button
+		startStopButton = new StartStopButton();
+		GridBagConstraints gbc6 = new GridBagConstraints();
+		gbc6.gridx = 0;
+		gbc6.gridy = 5;
+		gbc6.gridwidth = 1;
+		gbc6.gridheight = 1;
+		gbc6.weightx = 1;
+		gbc6.weighty = 2;
+		gbc6.anchor = GridBagConstraints.BASELINE_LEADING;
+		
+		this.add(startStopButton, gbc6);
+		startStopButton.setVisible( (openningMode == AddMapDialogMode.ADD) ? false : true );
+		
+		// add algorithm combo box
+		algorithmComboBox = new JComboBox<PositionLocalizationAlgorithmType>();
+		algorithmComboBox.addItem(PositionLocalizationAlgorithmType.PROBABILITY_BASED);
+		algorithmComboBox.addItem(PositionLocalizationAlgorithmType.PROXIMITY);
+		algorithmComboBox.addActionListener(new AlgorithmComboBoxActionListener());
+		GridBagConstraints gbc7 = new GridBagConstraints();
+		gbc7.gridx = 0;
+		gbc7.gridy = 4;
+		gbc7.gridwidth = 2;
+		gbc7.gridheight = 1;
+		gbc7.weightx = 1;
+		gbc7.weighty = 2;
+		gbc7.anchor = GridBagConstraints.BASELINE_LEADING;
+		
+		this.add(algorithmComboBox, gbc7);
+		algorithmComboBox.setVisible( (openningMode == AddMapDialogMode.EDIT) ? true : false);
 		
 		// Add 'Upload' button
 		uploadButton = new JButton("Upload");
@@ -111,9 +195,11 @@ public class ParametersPanel extends JPanel{
 	            //This is where a real application would open the file.
 	            
 	            addMapDialog.setPreviewImage(file);
+	            addMapDialog.addCoordinateZeroView();
 	            
 	        } else {
 	            
+	        	logger.severe("Couldn't open the image.");
 	        }
 			
 		}
@@ -154,6 +240,93 @@ public class ParametersPanel extends JPanel{
 			
 	}
 	
+	private class RotateButtonsListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			if (e.getSource().equals(rotateReceiverClockwiseButton)) {
+				
+				// TODO: handle clockwise rotation
+				addMapDialog.getMapPreviewPanel().rotateReceiverViewInFocus(-ROTATE_AMOUNT);
+			} else { // counter clockwise
+				
+				// TODO: handle counter clockwise rotation
+				addMapDialog.getMapPreviewPanel().rotateReceiverViewInFocus(ROTATE_AMOUNT);
+			}
+			
+		}
+		
+	}
 	
+	private class StartStopButton extends JButton implements ActionListener {
+		
+		private static final long serialVersionUID = 1L;
+		private StartStopButtonState state;
+		private ImageIcon startIcon = new ImageIcon(Utilities.loadImage(START_ICON_IMAGE_PATH));
+		private ImageIcon stopIcon  = new ImageIcon(Utilities.loadImage(STOP_ICON_IMAGE_PATH));
+		
+		public StartStopButton() {
+			this.state = StartStopButtonState.STARTED;
+			this.setIcon(startIcon);
+			this.addActionListener(this);
+		}
+
+		private void toggle() {
+			
+			if (state == StartStopButtonState.STOPPED) {
+				
+				state = StartStopButtonState.STARTED;
+				this.setIcon(startIcon);
+				this.repaint();
+			} else {
+				
+				state = StartStopButtonState.STOPPED;
+				this.setIcon(stopIcon);
+				this.repaint();
+			}
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			toggle();	
+		}	
+	}
+	
+	
+	private class AlgorithmComboBoxActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			JComboBox<PositionLocalizationAlgorithmType> comboBox = ((JComboBox<PositionLocalizationAlgorithmType>) e.getSource());
+			ComboBoxModel<PositionLocalizationAlgorithmType> model = comboBox.getModel();
+		    int index = comboBox.getSelectedIndex();
+		    PositionLocalizationAlgorithmType selection = model.getElementAt(index);
+			
+		    Application.getApplication().setAlgorithm(createAlgorithmInstance(selection));
+		}
+		
+	}
+	
+	
+	private PositionLocalizationAlgorithm createAlgorithmInstance(PositionLocalizationAlgorithmType type) {
+		
+		switch (type) {
+	    
+    	case PROBABILITY_BASED: 
+    		// TODO create probability based algorithm instance and return it
+    		return null;
+    	
+    	case PROXIMITY: 
+    		// TODO create proximity based algorithm instance and return it
+    		return null;
+    		
+    	default: 
+    		return null;
+		}
+		
+	}
 
 }
