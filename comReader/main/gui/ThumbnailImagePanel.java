8dc9bd14247b40eb11237d5dd26c4c1b277bb047
daniloradenovic <dacha111@gmail.com>
2013-12-11 12:40:08 +0100

    Updated 'paintComponent' method of class 'ThumbnailImagePanel'.

--- comReader/main/gui/ThumbnailImagePanel.java
+++ comReader/main/gui/ThumbnailImagePanel.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
@@ -13,7 +14,7 @@ public class ThumbnailImagePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private static final int THUMBNAIL_IMAGE_WIDTH = 130;
-	private static final int THUMBNAIL_IMAGE_HEIGHT = 100;
+	private static final int THUMBNAIL_IMAGE_HEIGHT = 160;
 	private Image backgroundImage;
 	
 	public ThumbnailImagePanel(Image image) {
@@ -21,15 +22,19 @@ public class ThumbnailImagePanel extends JPanel {
 		this.setSize(THUMBNAIL_IMAGE_WIDTH, THUMBNAIL_IMAGE_HEIGHT);
 		this.setPreferredSize(new Dimension(THUMBNAIL_IMAGE_WIDTH, THUMBNAIL_IMAGE_HEIGHT));
 		this.backgroundImage = Utilities.createThumbnailImageForContainer((BufferedImage) image, THUMBNAIL_IMAGE_WIDTH, THUMBNAIL_IMAGE_HEIGHT);
-		
 	}
 	
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		// Draw the background image.
-	    g.drawImage(this.backgroundImage, 0, 0, this);
+		
+		// Draw the background image at the center of the panel
+		Graphics2D g2d = (Graphics2D) g;
+	    int x = (this.getWidth() - backgroundImage.getWidth(null)) / 2;
+	    int y = (this.getHeight() - backgroundImage.getHeight(null)) / 2;
+		
+	    g.drawImage(this.backgroundImage, x, y, this);
 	}
 	
 
