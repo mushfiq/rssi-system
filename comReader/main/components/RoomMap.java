b24b17ef4cc437c56b106f6400b56199028babe6
daniloradenovic <dacha111@gmail.com>
2013-12-14 14:21:58 +0100

    Updated class 'RoomMap' to fit new fields. Added getters/setters.

--- comReader/main/components/RoomMap.java
+++ comReader/main/components/RoomMap.java
@@ -1,4 +1,3 @@
-
 /*
  * File: RoomMap.java
  * Date				Author				Changes
@@ -23,148 +22,165 @@ import algorithm.helper.PointRoomMap;
  * @author Tommy Griese
  */
 public class RoomMap {
-	
+
 	/** The default granularity constant for the roommap. */
 	public static final double GRANULARITY_DEFAULT = 0.25;
-	
-	private static final double DEFAULT_SCALING = 100.0; // pixel to meter ratio
-	
+
 	/** The actual granularity for the roommap. */
 	private double granularity;
-	
+
 	/** The start value for the room map in x. */
 	private double xFrom;
-	
+
 	/** The end value for the room map in x. */
 	private double xTo;
-	
+
 	/** The start value for the room map in y. */
 	private double yFrom;
-	
+
 	/** The end value for the room map in y. */
 	private double yTo;
-	
+
 	/** The path to the image of the room map. */
 	private Image image;
-	
+
 	/** Each point of this list contains a weighted point in a room ({@link algorithm.helper.PointRoomMap}). */
 	private ArrayList<PointRoomMap> pointsRoomMap;
-	
-	private double zeroOffsetXInMeters;
-	private double zeroOffsetYInMeters;
+	private int id;
 	private String title;
 	private List<Receiver> receivers;
-	private double scaling;
 	private int lowerLeftMarkerOffsetXInPixels;
 	private int lowerLeftMarkerOffsetYInPixels;
 	private int upperRightMarkerOffsetXInPixels;
 	private int upperRightMarkerOffsetYInPixels;
 	private double ratioWidth;
 	private double ratioHeight;
-	
+	private double widthInMeters;
+	private double heightInMeters;
+
+	public RoomMap() {
+		pointsRoomMap = new ArrayList<PointRoomMap>();
+		title = "Title";
+		receivers = new ArrayList<Receiver>();
+	}
+
+	public ArrayList<PointRoomMap> getPointsRoomMap() {
+		return pointsRoomMap;
+	}
+
+	public void setPointsRoomMap(ArrayList<PointRoomMap> pointsRoomMap) {
+		this.pointsRoomMap = pointsRoomMap;
+	}
+
 	/**
 	 * Instantiates a new room map.
-	 *
-	 * @param xFrom the start value for the room map in x
-	 * @param xTo the end value for the room map in x
-	 * @param yFrom the start value for the room map in y
-	 * @param yTo the end value for the room map in y
-	 * @param granularity the granularity of the room map
-	 * @param pathToImage the path to the image
+	 * 
+	 * @param xFrom
+	 *            the start value for the room map in x
+	 * @param xTo
+	 *            the end value for the room map in x
+	 * @param yFrom
+	 *            the start value for the room map in y
+	 * @param yTo
+	 *            the end value for the room map in y
+	 * @param granularity
+	 *            the granularity of the room map
+	 * @param pathToImage
+	 *            the path to the image
 	 */
 	public RoomMap(double xFrom, double xTo, double yFrom, double yTo, double granularity, Image image) {
 		super();
 		this.xFrom = xFrom;
 		this.xTo = xTo;
-		
+
 		this.yFrom = yFrom;
 		this.yTo = yTo;
 		this.image = image;
 		this.granularity = granularity;
-		
-		
+
 		initialize();
 	}
-	
+
 	/**
 	 * Instantiates a new room map.
-	 *
-	 * @param xFrom the start value for the room map in x
-	 * @param xTo the end value for the room map in x
-	 * @param yFrom the start value for the room map in y
-	 * @param yTo the end value for the room map in y
-	 * @param pathToImage the path to the image of the room map
+	 * 
+	 * @param xFrom
+	 *            the start value for the room map in x
+	 * @param xTo
+	 *            the end value for the room map in x
+	 * @param yFrom
+	 *            the start value for the room map in y
+	 * @param yTo
+	 *            the end value for the room map in y
+	 * @param image
+	 *            Image object the path to the image of the room map
 	 */
 	public RoomMap(double xFrom, double xTo, double yFrom, double yTo, Image image) {
 		super();
 		this.xFrom = xFrom;
 		this.xTo = xTo;
-		
+
 		this.yFrom = yFrom;
 		this.yTo = yTo;
 		this.image = image;
-		
+
 		this.granularity = RoomMap.GRANULARITY_DEFAULT;
-		
+
 		initialize();
 	}
-	
-	public RoomMap(Image image, double zeroOffsetXInMeters,
-			double zeroOffsetYInMeters, String title, List<Receiver> receivers) {
+
+	public RoomMap(Image image, String title, List<Receiver> receivers) {
 		super();
 		this.image = image;
-		this.zeroOffsetXInMeters = zeroOffsetXInMeters;
-		this.zeroOffsetYInMeters = zeroOffsetYInMeters;
-		
-		if (title == null || title =="") {
+
+		if (title == null || title == "") {
 			this.title = new SimpleDateFormat("dd.MM.yyyy, HH:ss").format(new Date());
 		} else {
 			this.title = title;
 		}
-		
+
 		this.receivers = receivers;
-		this.scaling = 1;
 	}
 
 	/**
 	 * Gets the start value for the room map in x.
-	 *
+	 * 
 	 * @return the x from
 	 */
 	public double getXFrom() {
 		return this.xFrom;
 	}
-	
+
 	/**
 	 * Gets the end value for the room map in x.
-	 *
+	 * 
 	 * @return the x to
 	 */
 	public double getXTo() {
 		return this.xTo;
 	}
-	
+
 	/**
 	 * Gets the start value for the room map in y.
-	 *
+	 * 
 	 * @return the y from
 	 */
 	public double getYFrom() {
 		return this.yFrom;
 	}
-	
+
 	/**
 	 * Gets the end value for the room map in y.
-	 *
+	 * 
 	 * @return the y to
 	 */
 	public double getYTo() {
 		return this.yTo;
 	}
-	
+
 	/**
 	 * Gets the image.
-	 *
+	 * 
 	 * @return image Image object
 	 */
 	public Image getImage() {
@@ -173,13 +189,14 @@ public class RoomMap {
 
 	/**
 	 * Sets the image.
-	 *
-	 * @param image Image object
+	 * 
+	 * @param image
+	 *            Image object
 	 */
 	public void setPathToImage(Image image) {
 		this.image = image;
 	}
-	
+
 	/**
 	 * Creates an array list of the room map with weighted points ({@link algorithm.helper.PointRoomMap}).
 	 */
@@ -192,19 +209,19 @@ public class RoomMap {
 			}
 		}
 	}
-	
+
 	/**
 	 * Gets the room map points.
-	 *
+	 * 
 	 * @return the room map points
 	 */
 	public ArrayList<PointRoomMap> getRoomMapPoints() {
 		return this.pointsRoomMap;
 	}
-	
+
 	/**
 	 * Gets the granularity.
-	 *
+	 * 
 	 * @return the granularity
 	 */
 	public double getGranularity() {
@@ -217,6 +234,109 @@ public class RoomMap {
 
 	public List<Receiver> getReceivers() {
 		return receivers;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getLowerLeftMarkerOffsetXInPixels() {
+		return lowerLeftMarkerOffsetXInPixels;
+	}
+
+	public void setLowerLeftMarkerOffsetXInPixels(int lowerLeftMarkerOffsetXInPixels) {
+		this.lowerLeftMarkerOffsetXInPixels = lowerLeftMarkerOffsetXInPixels;
+	}
+
+	public int getLowerLeftMarkerOffsetYInPixels() {
+		return lowerLeftMarkerOffsetYInPixels;
+	}
+
+	public void setLowerLeftMarkerOffsetYInPixels(int lowerLeftMarkerOffsetYInPixels) {
+		this.lowerLeftMarkerOffsetYInPixels = lowerLeftMarkerOffsetYInPixels;
+	}
+
+	public int getUpperRightMarkerOffsetXInPixels() {
+		return upperRightMarkerOffsetXInPixels;
+	}
+
+	public void setUpperRightMarkerOffsetXInPixels(int upperRightMarkerOffsetXInPixels) {
+		this.upperRightMarkerOffsetXInPixels = upperRightMarkerOffsetXInPixels;
+	}
+
+	public int getUpperRightMarkerOffsetYInPixels() {
+		return upperRightMarkerOffsetYInPixels;
+	}
+
+	public void setUpperRightMarkerOffsetYInPixels(int upperRightMarkerOffsetYInPixels) {
+		this.upperRightMarkerOffsetYInPixels = upperRightMarkerOffsetYInPixels;
+	}
+
+	public double getRatioWidth() {
+		return ratioWidth;
+	}
+
+	public void setRatioWidth(double ratioWidth) {
+		this.ratioWidth = ratioWidth;
+	}
+
+	public double getRatioHeight() {
+		return ratioHeight;
+	}
+
+	public void setRatioHeight(double ratioHeight) {
+		this.ratioHeight = ratioHeight;
+	}
+
+	public double getWidthInMeters() {
+		return widthInMeters;
+	}
+
+	public void setWidthInMeters(double widthInMeters) {
+		this.widthInMeters = widthInMeters;
+	}
+
+	public double getHeightInMeters() {
+		return heightInMeters;
+	}
+
+	public void setHeightInMeters(double heightInMeters) {
+		this.heightInMeters = heightInMeters;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+
+		return "Id: " + id + "\ntitle: " + title + "\nlower left marker offset x (px): "
+				+ lowerLeftMarkerOffsetXInPixels + "\nlower left marker offset y (px): "
+				+ lowerLeftMarkerOffsetYInPixels + "\nupper right marker offset x (px): "
+				+ upperRightMarkerOffsetXInPixels + "\nupper right marker offset y (px): "
+				+ upperRightMarkerOffsetYInPixels + "\nratio width (px/m): " + ratioWidth
+				+ "\nratio height (px/m): " + ratioHeight + "\nmap width (m): " + widthInMeters
+				+ "\nmap height (m): " + heightInMeters;
+	}
+	
+	public void addReceiver(Receiver receiver) {
+		receivers.add(receiver);
+	}
+	
+	public void removeReceiver(Receiver receiver) {
+		
+		int listSize = receivers.size();
+		for (int i = 0; i < listSize; i++) {
+			if (receivers.get(i).getID() == receiver.getID()) {
+				receivers.remove(i);
+				break;
+			}	
+		}
 	}
 
 }
