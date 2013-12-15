8a7280696961b0c1da4d9dd1765f4ac65cf7516b
Mushfiq <mushfiq.moon@gmail.com>
2013-12-15 02:39:24 +0100

    added curl call for user auth system

--- RESTApi/restApp/dummyData/curl.sh
+++ RESTApi/restApp/dummyData/curl.sh
@@ -3,10 +3,12 @@
 #POST WATCH EXAMPLE:
 
 curl -i -H "Content-Type: application/json" -X POST -d '{"mapId": 2, "watchId": "12", "x": 12.3, "y": 3.4}'  http://localhost:8000/api/v1/watch/
-
 #POST MAP EXAMPLE:
 
 curl -i -H "Content-Type: application/json" -X POST -d '{"mapId": 2, "receiverId": "12", "width": 12, "height": 3, "scaling": 1.5, "offsetX":23, "offsetY": 31}'  http://localhost:8000/api/v1/map/
+
+
+curl  -H "Content-Type: image/png"  -d '{"mapId": 0, "receiverId": "0", "x": 0.3, "y": 0.3}' -F "image=@sample.png" http://localhost:8000/api/v1/map/?access_key=5298fe82fb21ab1b8b72533
 
 #POST RECEIVER EXAMPLE:
 
@@ -34,5 +36,10 @@ curl -i -H "Content-Type: application/json" -X PUT -d '{"mapId": 0,"receiverId":
 
 curl -i -H "Content-Type: application/json" -X DELETE http://localhost:8000/api/v1/receiver/52988ae6fb21ab0abf74409f/
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#USER AUTH EXAMPLE:
+
+ curl  -d "username=name&password=word"  http://localhost:8000/api/v1/authenticate/
 
 
