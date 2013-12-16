80c6b2071195c7ad005c940ea58f1f07256af4be
daniloradenovic <dacha111@gmail.com>
2013-12-16 10:08:11 +0100

    Updated documentation for 'MapsPanel' class.

--- comReader/main/gui/MapsPanel.java
+++ comReader/main/gui/MapsPanel.java
@@ -1,3 +1,7 @@
+/*
+ * 
+ * 
+ */
 package gui;
 
 import java.awt.Color;
@@ -10,35 +14,37 @@ import javax.swing.JPanel;
 import main.Application;
 import components.RoomMap;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class MapsPanel.
+ * Contains <code>MapItem</code> objects that represent available <code>RoomMap</code>s in the data source.
+ * 
+ * @author Danilo
+ * @see RoomMap
  */
-public class MapsPanel extends JPanel{
+public class MapsPanel extends JPanel {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
-	
+
 	/** The Constant MAPS_PANEL_WIDTH. */
-	private static final int MAPS_PANEL_WIDTH  = 620;
-	
-	/** The Constant MAPS_PANEL_HEIGHT. */
-	private static final int MAPS_PANEL_HEIGHT = 500;
-	
-	/** The Constant WRAP_LAYOUT_FIX_HEIGHT. */
+	private static final int MAPS_PANEL_WIDTH = 620;
+
+	/** The Constant WRAP_LAYOUT_FIX_HEIGHT. Used instead of <code>MAPS_LAYOUT_HEIGHT</code> */
 	private static final int WRAP_LAYOUT_FIX_HEIGHT = 1;
-	
+
+	/** The Constant GRAY_COLOUR. */
+	private static final int GRAY_COLOUR = 230;
+
 	/**
-	 * Instantiates a new maps panel.
+	 * Instantiates a new <code>MapsPanel</code>.
 	 */
 	public MapsPanel() {
-		
-		setBackground(new Color(230, 230, 230));
+
+		setBackground(new Color(GRAY_COLOUR, GRAY_COLOUR, GRAY_COLOUR));
 		setLayout(new WrapLayout(FlowLayout.LEADING));
 		this.setSize(new Dimension(MAPS_PANEL_WIDTH, WRAP_LAYOUT_FIX_HEIGHT));
 		// TODO: obtain all maps using MapDAO, iterate over them and add MapItems
 		List<RoomMap> allMaps = Application.getApplication().getMapDAO().getAllMaps();
-		
+
 		for (RoomMap roomMap : allMaps) {
 			MapItem item = new MapItem(roomMap, this);
 			this.add(item);
@@ -46,8 +52,12 @@ public class MapsPanel extends JPanel{
 		this.revalidate();
 	}
 
+	/**
+	 * Refreshes map items. This method is needed when new <code>RoomMap</code>s are added to the data source and we
+	 * want to display newly added <code>RoomMap</code>s.
+	 */
 	public void refreshMapItems() {
-		
+
 		removeAll();
 		List<RoomMap> allMaps = Application.getApplication().getMapDAO().getAllMaps();
 		for (RoomMap roomMap : allMaps) {
