80f4e1cdf4ea7ecbde750f95dd84dc74109516ad
daniloradenovic <dacha111@gmail.com>
2013-12-16 10:02:38 +0100

    Updated documentation for 'WatchPositionData' class.

--- comReader/main/data/WatchPositionData.java
+++ comReader/main/data/WatchPositionData.java
@@ -1,3 +1,7 @@
+/*
+ * 
+ * 
+ */
 package data;
 
 import java.util.logging.Logger;
@@ -5,67 +9,102 @@ import java.util.logging.Logger;
 import utilities.Utilities;
 import algorithm.helper.Point;
 
-
 /**
- * Contains the information about watch x and y coordinates, watch id and the 
- * time when the calculation occurred. 
+ * Contains the information about watch x and y coordinates, watch id and the time when the calculation occurred.
+ * 
+ * @author Danilo
  */
 public class WatchPositionData {
 
-	/** The logger. */
+	/** <code>Logger</code> object. */
+	@SuppressWarnings("unused")
 	private Logger logger;
-	
+
 	/** The watch id. */
 	private int watchId;
-	
-	/** 
-	 * The time when reading was calculated. This is also used in the database as the
-	 * time when the reading occurred.
+
+	/**
+	 * The time when reading was calculated. This is also used in the database as the time when the reading occurred.
 	 * 
 	 */
 	private long time;
-	
+
 	/** The position of the watch. */
 	private Point position;
-	
+
 	/**
 	 * Instantiates a new watch position data.
 	 */
 	public WatchPositionData() {
-		
 		logger = Utilities.initializeLogger(this.getClass().getName());
 	}
 
+	/**
+	 * Gets the watch id.
+	 * 
+	 * @return the watch id
+	 */
 	public int getWatchId() {
 		return watchId;
 	}
 
+	/**
+	 * Sets the watch id.
+	 * 
+	 * @param watchId
+	 *            the new watch id
+	 */
 	public void setWatchId(int watchId) {
 		this.watchId = watchId;
 	}
 
+	/**
+	 * Gets the time.
+	 * 
+	 * @return the time
+	 */
 	public long getTime() {
 		return time;
 	}
 
+	/**
+	 * Sets the time.
+	 * 
+	 * @param time
+	 *            the new time
+	 */
 	public void setTime(long time) {
 		this.time = time;
 	}
 
+	/**
+	 * Gets the position.
+	 * 
+	 * @return the position
+	 */
 	public Point getPosition() {
 		return position;
 	}
 
+	/**
+	 * Sets the position.
+	 * 
+	 * @param position
+	 *            the new position
+	 */
 	public void setPosition(Point position) {
 		this.position = position;
 	}
 
 	/**
 	 * Instantiates a new watch position data.
-	 *
-	 * @param watchId the watch id
-	 * @param time the time
-	 * @param position the position
+	 * 
+	 * @param watchId
+	 *            the watch id
+	 * @param time
+	 *            the time
+	 * @param position
+	 *            the position
 	 */
 	public WatchPositionData(int watchId, long time, Point position) {
 		super();
@@ -74,13 +113,14 @@ public class WatchPositionData {
 		this.position = position;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		
 		return "Watch id: " + watchId + ", time: " + time + ", position: " + position.toString();
 	}
-	
+
 }
