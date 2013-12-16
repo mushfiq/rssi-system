1a24d52dc42a8e175c24bd0e8e7df85e6647ba31
daniloradenovic <dacha111@gmail.com>
2013-12-16 10:06:37 +0100

    Updated documentation for 'ImageFilter' class.

--- comReader/main/gui/ImageFilter.java
+++ comReader/main/gui/ImageFilter.java
@@ -1,51 +1,62 @@
+/*
+ * 
+ * 
+ */
 package gui;
 
 import java.io.File;
 
+import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 
-
 /**
- *  ImageFilter extends FileFilter and is supplied to the
- *  JFileChooser in order to display only specific files 
- *  and directories. Here it is used to filter everything
- *  except directories (so that the user can browse through 
- *  file system) and .png images.
+ * This class extends <code>FileFilter</code> and is supplied to the <code>JFileChooser</code> in order to display only
+ * specific files and directories. Here it is used to filter everything except directories (so that the user can browse
+ * through the file system) and .png images.
+ * 
+ * @author Danilo
+ * @see JFileChooser
  */
 public class ImageFilter extends FileFilter {
-	
-    /** 
-     * Overridden method in order to display only 
-     * directories and .png files.
-     * 
-     * @param file One of the files in the file system
-     * @return Is file accepted
-     * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
-     */
-    public boolean accept(File file) {
-    	
-        if (file.isDirectory()) {
-            return true;
-        }
 
-        String extension = null;
-        String fileName = file.getName();
-        
-        int i = fileName.lastIndexOf('.');
+	/**
+	 * Overridden method in order to display only directories and .png files.
+	 * 
+	 * @param file
+	 *            One of the <code>File</code>s in the file system
+	 * @return <code>boolean</code> is file accepted
+	 * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
+	 */
+	@Override
+	public boolean accept(File file) {
 
-        if (i > 0 &&  i < fileName.length() - 1) {
-        	extension = fileName.substring(i + 1).toLowerCase();
-        }
-        if (extension != null) {
-        	
-            return (extension.equals("png") ? true : false);
-        }
+		if (file.isDirectory()) {
+			return true;
+		}
 
-        return false;
-    }
+		String extension = null;
+		String fileName = file.getName();
 
-    
-    public String getDescription() {
-        return "PNG files only";
-    }
+		int i = fileName.lastIndexOf('.');
+
+		if (i > 0 && i < fileName.length() - 1) {
+			extension = fileName.substring(i + 1).toLowerCase();
+		}
+		if (extension != null) {
+
+			return (extension.equals("png") ? true : false);
+		}
+
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.filechooser.FileFilter#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		return "PNG files only";
+	}
 }
