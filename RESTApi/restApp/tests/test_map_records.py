381b8368d973f8bc63d6ece0882f50434a04d4bd
Mushfiq <mushfiq.moon@gmail.com>
2014-01-06 19:38:58 +0100

    updated map test suit

--- RESTApi/restApp/tests/test_map_records.py
+++ RESTApi/restApp/tests/test_map_records.py
@@ -1,12 +1,29 @@
-from tastypie.test import ResourceTestCase      
-class MapResourceTest(ResourceTestCase):
+import requests
+import unittest
+import json      
+
+from configuration import BASE_URL, OPTIONS, HEADERS
+
+class MapResourceTest(unittest.TestCase):
     
-    def setUp(slef):
-        super(EntryResourceTest, self).setUp()
+    def setUp(self):
+        self.base_url = BASE_URL+'map/'
+        data = {"mapId": 2, "receiverId": "12", "width": 12, "height": 3, "scalingX": 1.5, "scalingY": 2.6, "offsetX":23, "offsetY": 31}
+        self.response = requests.post(self.base_url, data=json.dumps(data), headers=HEADERS, params=OPTIONS)
+        self.testUrl = self.response.headers.get('location')
         
-    def test_get_list_json(self):
-        resp = self.api_client.get('/api/v1/map/', format=json)
-        self.assertValidJSONResponse(resp)
-        
+    def testInsertMap(self):
+        self.assertTrue(201, self.response.status_code)
+
+    def testUpdateMap(self):
+        data = {"mapId": 2, "receiverId": "102", "width": 102, "height": 102, "scalingX": 125, "offsetX":23, "offsetY": 31}
+        response = requests.put(self.testUrl, data=json.dumps(data), headers=HEADERS, params=OPTIONS )
+        self.assertTrue(204, response.status_code)
+
     def tearDown(self):
-        return
+        response = requests.delete(self.testUrl, params=OPTIONS)
+        self.assertTrue(204, response.status_code)
+        
+
+if __name__=='__main__':
+    unittest.main()
