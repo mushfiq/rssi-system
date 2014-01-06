578966f401bbd4c01c876952780832e10022cea6
Mushfiq <mushfiq.moon@gmail.com>
2014-01-06 20:52:48 +0100

    added doc string for runner.py

--- RESTApi/restApp/tests/runner.py
+++ RESTApi/restApp/tests/runner.py
@@ -4,7 +4,7 @@ from test_watch_records import watchResourceTest
 from test_map_records import MapResourceTest
 from test_receiver_records import ReceiverResourceTest
 
-//run this file for running all the test cases at a time. To add new test suit import the suit.
+#run this file for running all the test cases at a time. To add new test suit import the suit.
 
 if __name__=='__main__':
     unittest.main()
