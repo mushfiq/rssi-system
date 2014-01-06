a8c5fb28d1fc5e702f14a6e0166a6bff010b0680
Mushfiq <mushfiq.moon@gmail.com>
2014-01-06 19:44:38 +0100

    added configuration  doc string

--- RESTApi/restApp/tests/configuration.py
+++ RESTApi/restApp/tests/configuration.py
@@ -1,5 +1,7 @@
 ''' this file containts base configuration for 
     running the test cases for the backend
+    make sure backend rest api is running and mongodb is running
+    if you want to test with production checnge BASE_URL with http://shironambd.com/api/v1/
 '''
 
 OPTIONS = {'access_key':'5298fe82fb21ab1b8b725333', 'format':'json'}
