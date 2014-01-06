31f63b91674a38919626b0de9f84100a52eb2b26
Mushfiq <mushfiq.moon@gmail.com>
2014-01-06 19:32:04 +0100

    added test configuration file

---
+++ RESTApi/restApp/tests/configuration.py
@@ -0,0 +1,9 @@
+''' this file containts base configuration for 
+    running the test cases for the backend
+'''
+
+OPTIONS = {'access_key':'5298fe82fb21ab1b8b725333', 'format':'json'}
+BASE_URL = 'http://localhost:8000/api/v1/'
+HEADERS = {'content-type': 'application/json'} 
+
+
