6aba03b4e56ff504fa676b7a3f06447e556c48af
Mushfiq <mushfiq.moon@gmail.com>
2014-01-06 19:32:21 +0100

    added new test runner

---
+++ RESTApi/restApp/tests/runner.py
@@ -0,0 +1,9 @@
+import unittest
+from test_image_retreive import ImageRetreiveTest
+from test_watch_records import watchResourceTest
+from test_map_records import MapResourceTest
+from test_receiver_records import ReceiverResourceTest
+
+if __name__=='__main__':
+    unittest.main()
+
