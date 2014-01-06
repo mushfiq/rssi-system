07d28355d370b9a6bc79092effb698535cbb65ba
Mushfiq <mushfiq.moon@gmail.com>
2014-01-06 19:58:38 +0100

    added code comment for runner.py

--- RESTApi/restApp/tests/runner.py
+++ RESTApi/restApp/tests/runner.py
@@ -4,6 +4,8 @@ from test_watch_records import watchResourceTest
 from test_map_records import MapResourceTest
 from test_receiver_records import ReceiverResourceTest
 
+//run this file for running all the test cases at a time. To add new test suit import the suit.
+
 if __name__=='__main__':
     unittest.main()
 
