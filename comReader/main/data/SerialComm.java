c8d31947db94521e2fe3475ec871ce4dcf4f36b5
daniloradenovic <dacha111@gmail.com>
2014-01-13 11:45:27 +0100

    Fixed a warning in class "SerialComm".

--- comReader/main/data/SerialComm.java
+++ comReader/main/data/SerialComm.java
@@ -14,13 +14,14 @@ import java.util.*;
  * 
  * 
  * */
-public class SerialComm implements gnu.io.SerialPortEventListener{
-	
-	SerialPort serialPort; 
+public class SerialComm implements gnu.io.SerialPortEventListener {
+
+	SerialPort serialPort;
 	InputStream inputStream;
 	OutputStream outputStream;
 	public int isOpen = 0;
-	
+
+	@SuppressWarnings("unchecked")
 	public Vector<String> getPortList() {
 		Enumeration<CommPortIdentifier> portList;
 		Vector<String> portVect = new Vector<String>();
@@ -33,57 +34,68 @@ public class SerialComm implements gnu.io.SerialPortEventListener{
 				portVect.add(portId.getName());
 			}
 		}
-		//System.out.println(portVect.size() + " found Ports:");
+		// System.out.println(portVect.size() + " found Ports:");
 		for (int i = 0; i < portVect.size(); i++) {
-			//System.out.println(i + "  : " + portVect.elementAt(i));
+			// System.out.println(i + "  : " + portVect.elementAt(i));
 		}
-		
+
 		return portVect;
 	} // get Port list
-	
-	public void closeSio() {
-            inputStream = null;
-            outputStream = null;
-            serialPort.close();
-        }
 
+	public void closeSio() {
+		inputStream = null;
+		outputStream = null;
+		serialPort.close();
+	}
+
+	@SuppressWarnings("rawtypes")
 	public boolean openSio(int aPortListNumber, int aBaud) {
- 		CommPortIdentifier portId = null;
+		CommPortIdentifier portId = null;
 		Enumeration portList;
 		portList = CommPortIdentifier.getPortIdentifiers();
 		while (portList.hasMoreElements()) {
 			portId = (CommPortIdentifier) portList.nextElement();
-			if (aPortListNumber == 0) break;
+			if (aPortListNumber == 0)
+				break;
 			aPortListNumber--;
-			} 
-		//System.out.println("Oeffnen von Port Nr "+aPortListNumber);
-		if (portId == null) {/*System.out.println("Port nicht gefunden");*/return false;};
-// System.out.println("pos1");
+		}
+		// System.out.println("Oeffnen von Port Nr "+aPortListNumber);
+		if (portId == null) {/* System.out.println("Port nicht gefunden"); */
+			return false;
+		}
+		;
+		// System.out.println("pos1");
 		try {
 			serialPort = (SerialPort) portId.open("Serial_Test", 1000);
-		} catch (PortInUseException e) {/*System.out.println("Port in Use: "+e.toString());*/ }
-// tem.out.println("pos2");
+		} catch (PortInUseException e) {/* System.out.println("Port in Use: "+e.toString()); */
+		}
+		// tem.out.println("pos2");
 		if (serialPort != null)
-		try {
-			inputStream = serialPort.getInputStream();
-		} catch (IOException e) {/*System.out.println("GetInpStream: "+e.toString()); */}
+			try {
+				inputStream = serialPort.getInputStream();
+			} catch (IOException e) {/* System.out.println("GetInpStream: "+e.toString()); */
+			}
 		if (inputStream != null)
-		try {
-			serialPort.addEventListener(this);
-		} catch (TooManyListenersException e) {/*System.out.println("ToMAnyListeners: "+e.toString());*/ }
-		if (serialPort != null) serialPort.notifyOnDataAvailable(true);
-		if (serialPort != null) try {
-			serialPort.setSerialPortParams(aBaud, SerialPort.DATABITS_8, 
-						   SerialPort.STOPBITS_1, 
-						   SerialPort.PARITY_NONE);
-		} catch (UnsupportedCommOperationException e) {/*System.out.println("Uns.Comm.Op.Ex.: "+e.toString());*/ }
+			try {
+				serialPort.addEventListener(this);
+			} catch (TooManyListenersException e) {/* System.out.println("ToMAnyListeners: "+e.toString()); */
+			}
+		if (serialPort != null)
+			serialPort.notifyOnDataAvailable(true);
+		if (serialPort != null)
+			try {
+				serialPort.setSerialPortParams(aBaud, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+			} catch (UnsupportedCommOperationException e) {/* System.out.println("Uns.Comm.Op.Ex.: "+e.toString()); */
+			}
 
-		if (serialPort != null) try {
-			outputStream = serialPort.getOutputStream();
-		} catch (IOException e) {/*System.out.println("IOExcp: "+e.toString());*/ }
+		if (serialPort != null)
+			try {
+				outputStream = serialPort.getOutputStream();
+			} catch (IOException e) {/* System.out.println("IOExcp: "+e.toString()); */
+			}
 
-                return (inputStream != null);
-
+		return (inputStream != null);
 
 	}
 
@@ -103,45 +115,44 @@ public class SerialComm implements gnu.io.SerialPortEventListener{
 			break;
 		case SerialPortEvent.DATA_AVAILABLE:
 
-
-		break;
+			break;
 		} // switch
-	}  // method serialEvent 
-        
-    public void myWrite(String s) {
-                try {
+	} // method serialEvent
 
-                    int len = s.length();
-                    /*System.out.print("Send "+len+" numbers: ");*/
-                    for(int i = 0; i<len;i++) {
-			int c = s.charAt(i);
-                       /* System.out.printf("%02X ", c);*/
-			outputStream.write(s.charAt(i));
-                    }
-                    /*System.out.println();*/
-                    
-                } catch (IOException e) {
-                   /* System.err.println("IOException: "  + e.getMessage());*/        
-                }  // catch
-            }
- 
-// ------ must implement this method ...
+	@SuppressWarnings("unused")
+	public void myWrite(String s) {
+		try {
+
+			int len = s.length();
+			/* System.out.print("Send "+len+" numbers: "); */
+			for (int i = 0; i < len; i++) {
+				int c = s.charAt(i);
+				/* System.out.printf("%02X ", c); */
+				outputStream.write(s.charAt(i));
+			}
+			/* System.out.println(); */
+
+		} catch (IOException e) {
+			/* System.err.println("IOException: " + e.getMessage()); */
+		} // catch
+	}
+
+	// ------ must implement this method ...
 	public int readSio() throws java.io.IOException {
 		byte[] readBuffer = new byte[1];
 		int c;
 		int numBytes = 0;
-//		try {
-                if (inputStream != null)
-		   if (inputStream.available() > 0) {
-			numBytes = inputStream.read(readBuffer);
-			} 
-//		catch (IOException e) {}
-		if (numBytes ==1) {
-			c = (readBuffer[0] >= 0 ? readBuffer[0] : readBuffer[0] +256);
- 			return c;
-		} 
-		else return -1;		
+		// try {
+		if (inputStream != null)
+			if (inputStream.available() > 0) {
+				numBytes = inputStream.read(readBuffer);
+			}
+		// catch (IOException e) {}
+		if (numBytes == 1) {
+			c = (readBuffer[0] >= 0 ? readBuffer[0] : readBuffer[0] + 256);
+			return c;
+		} else
+			return -1;
 	}
-
 
 }
